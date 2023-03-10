95c7d42a165d3fe785135184ff775550463e3e05
Saba Ahmad <sabaahmad895@gmail.com>
2023-03-10 18:21:36 +0500

    morse code decode

---
+++ morsecode.rb
@@ -0,0 +1,20 @@
+MORSE_CODE = {
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
+
+def decode_char(morse_char)
+  MORSE_CODE[morse_char]
+end
+
+def decode_word(morse_word)
+  morse_word.split.map { |morse_char| decode_char(morse_char) }.join
+end
+
+def decode(message)
+  message.split('   ').map { |morse_word| decode_word(morse_word) }.join(' ')
+end
